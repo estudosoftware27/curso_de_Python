48679059cbe4ac56bd92dc071f6b08d6c25de6e4
estudo_software27 <estudosoftware27@gmail.com>
2025-09-12 12:08:24 -0300

    Add files via upload

---
+++ name.py
@@ -0,0 +1,22 @@
+# Strings
+# O que é uma string?
+# Uma string é uma série de caracteres.
+# Tudo que estiver entre aspas é considerada uma string em Python.
+# Você pode usar aspas simples --> ' '
+# ou você pode usar aspas duplas --> "  "
+
+# Usando aspas duplas --> "Só sei que nada sei."
+# usando aspas simples --> 'Só sei que nada sei.'
+
+# Essa flexibilidade permite usar aspas e apóstrofos em suas strings:
+mensagem = "Ela disse: 'O projeto está pronto', e então sorriu."
+print(mensagem)
+
+# Saída de impressão:
+# Ela disse: 'O projeto está pronto', e então sorriu.
+
+mensagem = 'Ele respondeu: "Vamos começar agora mesmo!"'
+print(mensagem)
+
+# Saída de impressão:
+# Ele respondeu: "Vamos começar agora mesmo!"
