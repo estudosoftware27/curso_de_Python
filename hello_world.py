a672628dfc3539bd58d85b20f4b217483a5dbcd4
estudo_software27 <estudosoftware27@gmail.com>
2025-09-12 02:47:17 -0300

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,33 @@
+print("Hello Python world!")
+
+# Quando executar esse código, você deverá ver a saída a seguir:
+
+# Hello Python World!
+
+# Ao executar hello_world.py, o arquivo terminado em .py indica que é
+# um programa python.
+
+# Quando o interpretador vê a palavra print, ele exibe na tela o que
+# quer que esteja dentro dos parênteses.
+
+mensagem = "Hello Python World!"
+print(mensagem)
+
+# Executando esse programa vemos a mesma saída visto anteriormente:
+# Hello Python World!
+
+# Foi acrescentado uma váriavel chamada mensagem.
+# Toda variável armazena um valor.
+# O valor é a informação associada a essa variável.
+
+# mensagem --> é a variável
+# Hello Python World! --> é a informação associada a essa variável.
+
+# O python sempre manterá o controle do valor atual.
+# Podemos mudar o valor de uma variável em nosso programa a qualquer
+# momento.
+
+# Ao executar hello_world.py aparecerá duas linhas na saída:
+
+mensagem = "Eu adoro estudar Python!"
+print(mensagem)
---
+++ treino_1.py
@@ -0,0 +1,31 @@
+# Exercício sobre Python
+# 1 - Mensagem simples:
+# Armazene uma mensagem em uma variável e,
+# em seguida, exiba essa mensagem.
+
+material_escolar = "caneta, lápis, caderno, borracha."
+print(material_escolar)
+
+# Essa será a saída no terminal:
+# caneta, lápis, caderno, borracha.
+
+# 2 - Mensagem simples:
+# Armazene uma mensagem em uma variável e, em seguida,
+# exiba essa mensagem. Então altere o valor de sua
+# variável para uma nova mensagem e mostre essa
+# nova mensagem.
+
+letra_musica = "Te amo e vou gritar pra todo mundo ouvir, ter você é meu desejo de viver."
+print(letra_musica)
+
+# Essa será a saída na tela:
+# Te amo e vou gritar pra todo mundo ouvir, ter você é meu desejo de viver.
+
+letra_musica = "Meu riso é tão feliz contigo, o meu melhor amigo é o meu amor. 💞"
+print(letra_musica)
+
+# Considerando que o python mantém o último valor atual:
+# Essa será a saída:
+# Meu riso é tão feliz contigo, o meu melhor amigo é o meu amor.
+
+# Concluído
