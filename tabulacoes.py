64ec418b9dde02d00c5365a019e1b076c9d1f28d
estudo_software27 <estudosoftware27@gmail.com>
2025-09-22 16:36:05 -0300

    Add files via upload

---
+++ tabulacoes.py
@@ -0,0 +1,13 @@
+# Acrescentando espaços em branco em strings
+# usando tabulações (\t) e quebras de linha (\n)
+
+print("Nome:\tJoão")
+print("Idade:\t25")
+print("Cidade:\tSão Paulo")
+
+print("Lista de Compras:")
+print("1. Maçã\n2. Banana\n3. Laranja")
+
+print("Felicidade")
+print("\tFelicidade")
+print("\nFelicidade")
