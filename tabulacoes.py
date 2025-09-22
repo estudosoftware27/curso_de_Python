5362ecab792793a668b68faa1f4dee564bc75ee4
estudo_software27 <estudosoftware27@gmail.com>
2025-09-22 16:41:05 -0300

    Add files via upload

--- tabulacoes.py
+++ tabulacoes.py
@@ -1,13 +1,26 @@
 # Acrescentando espaços em branco em strings
 # usando tabulações (\t) e quebras de linha (\n)
 
-print("Nome:\tJoão")
-print("Idade:\t25")
-print("Cidade:\tSão Paulo")
+print("Nome:\tJoão") # Usando tabulação para alinhar o texto
+print("Idade:\t25") # Usando tabulação para alinhar o texto
+print("Cidade:\tSão Paulo") # Usando tabulação para alinhar o texto
+# Saída:
+# Nome:   João
+# Idade:  25
+# Cidade: São Paulo
 
-print("Lista de Compras:")
-print("1. Maçã\n2. Banana\n3. Laranja")
+print("Lista de Compras:") # Sem tabulação ou quebra de linha
+print("1. Maçã\n2. Banana\n3. Laranja") # Com quebras de linha
+# Saída:
+# Lista de Compras:
+# 1. Maçã
+# 2. Banana
+# 3. Laranja
 
-print("Felicidade")
-print("\tFelicidade")
-print("\nFelicidade")
+print("Felicidade") # Sem tabulação ou quebra de linha
+print("\tFelicidade") # Com tabulação
+print("\nFelicidade") # Com quebra de linha
+# Saída:
+# Felicidade    
+#     Felicidade
+# Felicidade
