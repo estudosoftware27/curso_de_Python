85b303664291dca4ebbfb424033d26630b09aca8
estudo_software27 <estudosoftware27@gmail.com>
2025-09-22 17:03:31 -0300

    Add files via upload

---
+++ treino_2.py
@@ -0,0 +1,19 @@
+# Faça você mesmo:
+# Mensagem pessoal:
+# Armazene o nome de uma pessoa em uma variável e, então, 
+# apresente uma mensagem para essa pessoa. 
+# Sua mensagem deve ser simples, como "Olá Eric, 
+# gostaria de aprender um pouco de Python hoje?".
+
+nome = "Maria"
+print(f"Olá {nome}, gostaria de aprender um pouco de Python hoje?")
+# Saída esperada: Olá Maria, gostaria de aprender um pouco de Python hoje?
+
+nome1 = "João"
+print("Olá " + nome1 + ", gostaria de aprender um pouco de Python hoje?")
+# Saída esperada: Olá João, gostaria de aprender um pouco de Python hoje?
+
+nome2 = "Ana"
+mensagem = "gostaria de aprender um pouco de Python hoje?"
+print(f"Olá {nome2}, {mensagem}")
+# Saída esperada: Olá Ana, gostaria de aprender um pouco de Python hoje?
