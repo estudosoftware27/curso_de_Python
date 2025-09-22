6f7af0cea8e309b86e06bdb6f9a50a56ce2b563b
estudo_software27 <estudosoftware27@gmail.com>
2025-09-22 17:10:27 -0300

    Add files via upload

---
+++ treino_3.py
@@ -0,0 +1,18 @@
+# Letras maiúsculas e minúsculas:
+# Armazene o nome de uma pessoa em uma variável,
+# e então apresente o nome dessa pessoa em letras maiúsculas,
+# seguido do nome em letras minúsculas,
+# e, por fim, com a primeira letra de cada nome em maiúscula.
+
+nome = "francisca maria"
+print(nome.upper())  # FRANCISCA MARIA
+print(nome.lower())  # francisca maria  
+print(nome.title())  # Francisca Maria
+# Saída esperada:
+# FRANCISCA MARIA
+# francisca maria
+# Francisca Maria
+
+nome1 = "PEDRO GUSTAVO"
+print(nome1.lower())  # pedro gustavo
+# Saída esperada: pedro gustavo
