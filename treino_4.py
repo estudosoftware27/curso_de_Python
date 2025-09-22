1debcc0ad568101739b112447904684210c11b07
estudo_software27 <estudosoftware27@gmail.com>
2025-09-22 17:16:49 -0300

    Add files via upload

---
+++ treino_4.py
@@ -0,0 +1,7 @@
+# Citação famosa:
+# Encontre uma citação de uma pessoa famosa que você goste.
+# Exiba a citação e o nome da pessoa que a disse.
+
+citacao = "Posso todas as coisas naquele que me fortalece." 
+autor = "Paulo de Tarso"
+print(f'{autor} certa vez disse: "{citacao}"')
